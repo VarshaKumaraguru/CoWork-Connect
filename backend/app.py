cb8d244c25515a5f4ab649d479631e110688d68b
Varsha Kumaraguru <122372761+VarshaKumaraguru@users.noreply.github.com>
2025-06-20 21:29:08 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -20,7 +20,7 @@ load_dotenv()
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)  # Initialize Flask-Migrate
-CORS(app, supports_credentials=True, origins=[os.environ.get("FRONTEND_ORIGIN")])
+CORS(app, origins=["https://cowork-connect-2.onrender.com"], supports_credentials=True)
 
 # Initialize Flask-Mail for contact form
 contact_mail = Mail(app)
