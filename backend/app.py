c878ac25cdbb991296abb44eb7bb8a6720360c9e
Varsha Kumaraguru <122372761+VarshaKumaraguru@users.noreply.github.com>
2025-06-20 21:41:07 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -594,4 +594,4 @@ def contact():
         return jsonify({"error": "Failed to send email"}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
