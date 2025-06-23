3b35a2cc04493abea4a4bc80c70af78491c3a871
Varsha Kumaraguru <122372761+VarshaKumaraguru@users.noreply.github.com>
2025-06-23 13:51:16 +0530

    Update SignUp.jsx

--- frontend/src/pages/Auth/SignUp.jsx
+++ frontend/src/pages/Auth/SignUp.jsx
@@ -20,13 +20,11 @@ function Signup() {
       return;
     }
 
-    const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:5000";
-    const response = await fetch(`${BACKEND_URL}/signup`, {
-
+    const response = await fetch("http://localhost:5000/signin", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ 
-        username: username.toLowerCase(), // Convert to lowercase before sending
+        username: username.toLowerCase(),
         email, 
         password 
       }),
